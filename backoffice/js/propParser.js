647fabd65d61a071d602ca3310108dd322cca437
Halfpaw <thomaslecourt@hotmail.fr>
2014-11-27 14:48:37 +0100

    add transition in JSON

--- backoffice/js/propParser.js
+++ backoffice/js/propParser.js
@@ -1,10 +1,11 @@
 var img = function(name, desc){
     this.name = name;
     this.desc = desc;
+    this.transition = transition;
 };
 
 img.prototype.toString = function imgToString(){
-    return "\n\t{\n\t\t'name': '"+this.name+"',\n\t\t'desc': '"+this.desc+"'\n\t}";
+    return "\n\t{\n\t\t'name': '"+this.name+"',\n\t\t'desc': '"+this.desc+"'\n\t\t'transition': '"+this.transition+"'\n\t}";
 };
 
 var createJsonImages = function(){
@@ -20,7 +21,8 @@ var createJsonImages = function(){
             throw err;
 
         var propFile, imageFile;
-
+        var transitions = ['fade', 'translate'];
+        var transNumber = 0;
         var numberPropFiles = 0;
         var numberPushedFiles = 0;
 
@@ -34,14 +36,14 @@ var createJsonImages = function(){
                 propFile = files.shift();
             }
             numberPropFiles += 1;
-            (function(propFile, imageFile){
+            (function(propFile, imageFile, transNumber){
                 fs.readFile(path+propFile, function(err, data){
                     if(err)
                         throw err;
                     var str='';
                     str += descriptionPattern.exec(data);
                     str = str.replace("Description=","").replace("Description:","").replace("\n","");
-                    propList.push(new img(imageFile, str));
+                    propList.push(new img(imageFile, str, transitions[transNumber]));
                     numberPushedFiles += 1;
                     if(numberPushedFiles === numberPropFiles){
                         var msg = "{ imgs : ["+propList+" \n\t]\n}";
@@ -50,7 +52,8 @@ var createJsonImages = function(){
                         });
                     }
                 });
-            })(propFile, imageFile);
+            })(propFile, imageFile, transNumber);
+            transNumber = (transNumber+1)%2;
         }
     });
 };

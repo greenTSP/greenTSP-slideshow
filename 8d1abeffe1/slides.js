e79f33a208ef2c36a88950c307d760a3778c7963
François Monniot <francoismonniot@gmail.com>
2014-11-27 11:48:01 +0100

    display correct counter at initialization

--- 8d1abeffe1/slides.js
+++ 8d1abeffe1/slides.js
@@ -1,16 +1,4 @@
 (function() {
-  // img size: width="900" height="360"
-  /*
-  var _slides = {
-  id: '#slideshow',
-  slides: [
-  {src: '../gclcimages/bach_creek_river.jpg', caption: 'Un ruisseau ombragé'},
-  {src: '../gclcimages/canoe_water_nature.jpg', caption: 'Une invitation à naviguer paisiblement'},
-  {src: '../gclcimages/gclc.png', caption: 'le logo officiel du Green Code Lab Challenge'},
-  {src: '../gclcimages/guyana_sky_clouds.jpg', caption: 'Landscape in Amazonian Guyana'}
-  ]
-};
-  */
   var slidesData, counterSeparator, autoplayTime;
   var slidesNode, statusNode, prevNode, nextNode, counterNode, navNode;
   var slidesNodeList;
@@ -31,6 +19,10 @@
     return fig;
   }
 
+  function updateCounter() {
+    counterNode.textContent = (current + 1) + counterSeparator + slidesNodeList.length;
+  }
+
   function Slideshow(opts) {
     slidesData = opts.slides;
     autoplayTime = opts.autoplayTime || 3000;
@@ -44,11 +36,6 @@
     nextNode = root.querySelector('.next');
     counterNode = root.querySelector('.counter');
     navNode = root.querySelector('nav');
-
-    // Generate the slides
-    for(var i = 0; i < slidesData.length; i++) {
-      slidesNode.appendChild(createSlide(slidesData[i].src,slidesData[i].caption));
-    }
 
     // List of slides
     slidesNodeList = slidesNode.children;
@@ -66,7 +53,7 @@
 
     current = to;
 
-    counterNode.textContent = (current + 1) + counterSeparator + slidesNodeList.length;
+    updateCounter();
   };
 
   Slideshow.prototype.nextSlide = function nextSlide() {
@@ -79,10 +66,15 @@
   };
 
   Slideshow.prototype.init = function() {
-    var len = slidesNodeList.length;
+
+    // Generate the slides
+    for(var i = 0; i < slidesData.length; i++) {
+      slidesNode.appendChild(createSlide(slidesData[i].src,slidesData[i].caption));
+    }
 
     // Generate navigation
-    console.log('generating ' + len + ' links');
+    var len = slidesNodeList.length;
+
     for(var i = 0; i < len; i++) {
       var a = document.createElement('a');
       a.href = '#';
@@ -96,6 +88,9 @@
 
       navNode.appendChild(a)
     }
+
+    // Display where we are in that slideshow
+    updateCounter();
 
     // Set default classes
     slidesNodeList.item(current).classList.add('show');
@@ -128,24 +123,6 @@
 
     statusNode.classList.toggle('playing');
     statusNode.classList.toggle('stopped');
-  };
-
-  Slideshow.prototype.lazyLoad = function(index) {
-  	var prev, next;
-
-  	this.load(index);
-
-  	prev = (index===0)?slides.length:index-1;
-  	this.load(prev);
-
-  	next = ((index+1)===slides.length)?0:index+1;
-  	this.load(next);
-  };
-
-  Slideshow.prototype.load = function(index)
-  {
-	if(!slides[index].load)
-		document.getElementById(slides[index-1].id).setAttribute('src', slides[index].src);
   };
 
   var slideshow = new Slideshow(_slides);

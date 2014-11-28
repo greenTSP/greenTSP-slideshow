6dac53bbca36ff2a64d6c80ae5a53e995e7a0039
cberthou <d2_lothien@hotmail.fr>
2014-11-28 10:33:32 +0100

    Ok

--- 8d1abeffe1/upload.php
+++ 8d1abeffe1/upload.php
@@ -1,8 +1,8 @@
 <?php
 	$uploaddir = '../backoffice/js/tempimg/';
-	$uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
+	$uploadfile = $uploaddir.basename($_FILES['image']['name']);
 	echo '<pre>';
-	if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)){
+	if(move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)){
 		$command = 'node ../backoffice/js/run.js';
 		echo "Fichier téléchargé\n";
 	} else {

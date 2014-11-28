b278924f18add85719533d3391fae9af7eda52d5
cberthou <d2_lothien@hotmail.fr>
2014-11-28 10:25:21 +0100

    Added php upload page

---
+++ 8d1abeffe1/upload.php
@@ -0,0 +1,13 @@
+<?php
+	$uploaddir = '../backoffice/js/tempimg/';
+	$uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
+	echo '<pre>'
+	if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile){
+		$command = 'node ../backoffice/js/run.js';
+		echo "Fichier téléchargé\n";
+	} else {
+		echo "Erreur de téléchargement.";
+	}
+	echo '</pre>';
+
+?>

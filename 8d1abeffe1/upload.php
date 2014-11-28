deaff9f1accc3fcafc57dd9707ecccc8e6ce346b
cberthou <d2_lothien@hotmail.fr>
2014-11-28 12:02:36 +0100

    Cleaned upload.php and runUpload.js

--- 8d1abeffe1/upload.php
+++ 8d1abeffe1/upload.php
@@ -5,13 +5,11 @@
 	$finalname = "".time().".".$ext;
 	$desc = $_POST['description'];
 
-	
 	echo '<pre>';
 	if(move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)){
 		$command = "node /var/www/backoffice/js/runUpload.js '".$finalname."' '".$desc."' '".$uploadfile."'";
 		exec($command, $out);
 		echo "Fichier téléchargé\n";
-		var_dump($out);
 	} else {
 		echo "Erreur de téléchargement.";
 	}
--- backoffice/js/runUpload.js
+++ backoffice/js/runUpload.js
@@ -7,16 +7,14 @@ var finalName = process.argv[2],
 	
 process.chdir("/var/www/backoffice/js/");
 
-console.log(finalName + " " + desc + " " + initName );
 resizer.exportBackofficeImg(finalName, desc, initName, function(err){
 	if(err)
-		console.log(err);
+		throw err;
 	var command = 'rm '+initName;
 	console.log(command);
 	exec(command, function(err){
 		if(err)
 			throw err;
-		console.log('desc = ' + desc);
-		console.log('File removed');		
+		
 	});
 });

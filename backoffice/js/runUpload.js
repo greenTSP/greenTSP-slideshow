98535e2fa3726b0ec2d7677753b8dc9497b1c7a1
cberthou <d2_lothien@hotmail.fr>
2014-11-28 11:34:31 +0100

    fixed file upload

--- backoffice/js/runUpload.js
+++ backoffice/js/runUpload.js
@@ -1,10 +1,10 @@
 var resizer = require('./resizer.js');
 
-var finalname = process.argv[1],
+var finalName = process.argv[1],
 	desc = process.argv[2],
-	initname = process.argv[3];
+	initName = process.argv[3];
 
-resizer.exportBackofficeImg(finalname, desc, initname, function(){
+resizer.exportBackofficeImg(finalName, desc, initName, function(){
 	var command = 'rm '+initName;
 	console.log(command);
 	exec(command, function(err){
